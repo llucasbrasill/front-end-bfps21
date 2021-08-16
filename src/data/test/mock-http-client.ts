43dd62a413281cae55d1c1843dcafea09edce756
Lucas Sodre <lucas.sodre@casion.com.br>
2021-08-16 06:43:27 -0300

    refactor: change imports to use module

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,4 +1,4 @@
-import { HttpPostClient, HTtpPostParams } from '../protocols/http/http-post-client'
+import { HttpPostClient, HTtpPostParams } from '@/data/protocols/http/http-post-client'
 
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string
