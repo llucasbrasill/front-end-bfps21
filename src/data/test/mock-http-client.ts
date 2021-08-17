50bbd3d1952c72744391777f25c509906f1c3a45
Lucas Sodre <lucas.sodre@casion.com.br>
2021-08-17 18:49:57 -0300

    refactor: group imports in index files

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,5 +1,4 @@
-import { HttpPostClient, HTtpPostParams } from '@/data/protocols/http/http-post-client'
-import { HttpResponse, HttpStatusCode } from '@/data/protocols/http/http-response'
+import { HttpPostClient, HTtpPostParams, HttpResponse, HttpStatusCode } from '@/data/protocols/http'
 
 export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
   url?: string
--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,8 +1,6 @@
-import { Authentication, AuthenticationParams } from '@/domain/usecases/authentication'
-import { HttpPostClient } from '@/data/protocols/http/http-post-client'
-import { HttpStatusCode } from '@/data/protocols/http/http-response'
-import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-erros'
-import { UnexpectedError } from '@/domain/errors/unexpected-error'
+import { Authentication, AuthenticationParams } from '@/domain/usecases'
+import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
 import { AccountModel } from '@/domain/models/account-model'
 
 export class RemoteAuthentication implements Authentication {
