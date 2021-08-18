97b991a55f0dd133342f865117ece4aeb1a4726f
Lucas Sodre <lucas.sodre@casion.com.br>
2021-08-18 08:38:28 -0300

    feat: ensure AxiosHttpClient return  correct values

--- src/infrastructure/http/axios-http-client.ts
+++ src/infrastructure/http/axios-http-client.ts
@@ -1,8 +1,12 @@
-import { HTtpPostParams } from '@/data/protocols/http'
+import { HttpPostClient, HTtpPostParams, HttpResponse } from '@/data/protocols/http'
 import axios from 'axios'
 
-export class AxiosHttpClient {
-  async post (params: HTtpPostParams<any>): Promise<void> {
-    await axios.post(params.url, params.body)
+export class AxiosHttpClient implements HttpPostClient<any, any> {
+  async post (params: HTtpPostParams<any>): Promise<HttpResponse<any>> {
+    const httpResponse = await axios.post(params.url, params.body)
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data
+    }
   }
 }
