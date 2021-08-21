ad5fa4b8ff7d614eb547f02f1277253e1198204c
Lucas Sodre <lucas.sodre@casion.com.br>
2021-08-21 18:54:59 -0300

    test: ensure Signin call valitation with correct email

--- src/presentation/pages/login/signin/signin.spec.tsx
+++ src/presentation/pages/login/signin/signin.spec.tsx
@@ -1,30 +1,54 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react'
+import { Validation } from '@/presentation/protocols/valitation'
 import { SignIn } from '../../index'
 import { BrowserRouter } from 'react-router-dom'
 
 type SutTypes = {
   sut: RenderResult
+  validationSpy: Validation
 }
+
+class ValidationSpy implements Validation {
+  errorMessage: string
+  input: object
+  validate (input: object): string {
+    this.input = input
+    return this.errorMessage
+  }
+}
+
 const makeSut = (): SutTypes => {
+  const validationSpy = new ValidationSpy()
   const sut = render(
     <BrowserRouter>
-        <SignIn />
+        <SignIn validation={validationSpy} />
     </BrowserRouter>)
 
   return {
-    sut
+    sut,
+    validationSpy
   }
 }
 
 describe('Signnn Page', () => {
+  afterEach(cleanup)
   test('should start with initial state', () => {
     const { sut } = makeSut()
     const emailWrapper = sut.getByTestId('emailWrapper')
     expect(emailWrapper.childElementCount).toBe(3)
     const submitButtonEmail = sut.getByTestId('submitEmail') as HTMLButtonElement
     expect(submitButtonEmail.disabled).toBe(true)
-    const emailInput = sut.getByTestId('emailInput') as HTMLInputElement
+    const emailInput = sut.getByTestId('email') as HTMLInputElement
     expect(emailInput.value).toBe('')
+  })
+
+  test('should call Validation with correct values email', () => {
+    const { sut, validationSpy } = makeSut()
+    const emailInput = sut.getByTestId('email') as HTMLInputElement
+    fireEvent.input(emailInput, { target: { value: 'any_email' } })
+    expect(validationSpy.input).toEqual({
+      email: 'any_email'
+    })
   })
 })
