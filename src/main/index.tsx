7d34bd730f887f4b18a945cdb7ab7d5cc29ae88d
Lucas Sodre <lucas.sodre@casion.com.br>
2021-08-21 16:22:55 -0300

    feat: add main to index file sass css

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import '@/presentation/assets/sass/components/_buttons.scss'
+import '@/presentation/assets/sass/app.scss'
 import { Router } from '@/presentation/components'
 
 ReactDOM.render(
