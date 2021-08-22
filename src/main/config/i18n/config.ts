1c4a2c8e40b0a8dce97cb9f22e06bb2a0fe376a9
Lucas Sodre <lucas.sodre@casion.com.br>
2021-08-22 15:20:11 -0300

    chore: add i18n configs

---
+++ src/main/config/i18n/config.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import i18n from 'i18next'
+import ptBR from './ptBR/common.json'
+import en from './en/common.json'
+import { initReactI18next } from 'react-i18next'
+
+export const resources = {
+  en: {
+    translation: ptBR
+  },
+  pt: {
+    translation: en
+  }
+} as const
+
+// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+i18n.use(initReactI18next).init({
+  lng: 'pt',
+  resources
+})
--- src/main/index.tsx
+++ src/main/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import '@/presentation/assets/sass/app.scss'
 import { Router } from '@/presentation/components'
+import './config/i18n/config'
 
 ReactDOM.render(
     <Router />,
