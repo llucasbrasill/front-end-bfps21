1354ff9ec55289295689b505a4376c0cc403bf76
Lucas Sodre <lucas.sodre@casion.com.br>
2021-08-25 10:33:00 -0300

    test: ensure RequiredFieldValidation return falsy if field is not empty

--- src/validation/required-field/required-field-validation.spec.tsx
+++ src/validation/required-field/required-field-validation.spec.tsx
@@ -1,10 +1,16 @@
 import { RequiredFiedlError } from '@/validation/errors'
 import { RequiredFiedlValidation } from '@/validation/required-field/required-field-validation'
-
+import faker from 'faker'
 describe('RequiredFieldValidation', () => {
   test('should return error if field is empty', async () => {
     const sut = new RequiredFiedlValidation('email')
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFiedlError())
+  })
+
+  test('should return falsy if field is not empty', async () => {
+    const sut = new RequiredFiedlValidation('email')
+    const error = sut.validate(faker.random.word())
+    expect(error).toBeFalsy()
   })
 })
