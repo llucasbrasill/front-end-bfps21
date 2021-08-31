0c07deb15d3642b197f9499bc94cf40be021b2eb
Lucas Sodre <lucas.sodre@casion.com.br>
2021-08-31 15:26:25 -0300

    test: ensure EmailValidation return falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -8,4 +8,10 @@ describe('EmailValidation', () => {
     const error = sut.validate('')
     expect(error).toEqual(new InvalidFieldError())
   })
+
+  test('should return falsy if email is valid', () => {
+    const sut = new EmailValidation(faker.random.word())
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
+  })
 })
