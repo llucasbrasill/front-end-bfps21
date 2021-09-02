60868a3e043eda263df3fcff9fe8bfc1b1174bb0
Lucas Sodre <lucas.sodre@casion.com.br>
2021-09-02 02:02:25 -0300

    test: ensure MinLengthValidation returns if value is invalid

---
+++ src/validation/validators/min-length/min-length-validators.spec.ts
@@ -0,0 +1,10 @@
+import { InvalidFieldError } from '@/validation/errors'
+import { MinLenghtValidation } from './min-lenght-validators'
+
+describe('MinLenghtValidation', () => {
+  test('should return error if value is invalid', () => {
+    const sut = new MinLenghtValidation('field', 5)
+    const error = sut.validate('123')
+    expect(error).toEqual(new InvalidFieldError())
+  })
+})
