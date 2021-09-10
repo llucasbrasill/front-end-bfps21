5028064c3c9710c51d7addcd698c6312bcd0e59f
Lucas Sodre <lucas.sodre@casion.com.br>
2021-09-10 01:31:10 -0300

    test: ensure ValidationBuilder has an email  helper

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,10 +1,15 @@
 
-import { RequiredFiedlValidation } from '@/validation/validators'
+import { EmailValidation, RequiredFiedlValidation } from '@/validation/validators'
 import { ValidationBuilder as sut } from './validation-builder'
 
 describe('ValidationBuilder', () => {
   test('should return RequiredFieldValidation', () => {
     const validations = sut.field('any_field').required().build()
     expect(validations).toEqual([new RequiredFiedlValidation('any_field')])
+  })
+
+  test('should return EmailValidation', () => {
+    const validations = sut.field('any_field').email().build()
+    expect(validations).toEqual([new EmailValidation('any_field')])
   })
 })
