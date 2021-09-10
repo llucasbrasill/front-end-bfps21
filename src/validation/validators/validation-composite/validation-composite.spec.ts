c16b5a4ee8050ece20de76ff0140b14be190d9bd
Lucas Sodre <lucas.sodre@casion.com.br>
2021-09-10 00:54:24 -0300

    test: ensure ValidationComposite return the first error found

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -5,13 +5,14 @@ import { ValidationComposite } from './validation-composite'
 describe('ValidationComposite', () => {
   test('should return error if any validation fails', () => {
     const fieldValidationSpy = new FieldValidationSpy('any_field')
+    fieldValidationSpy.error = new Error('fisrt_arror_message')
     const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldValidationSpy2.error = new Error('any_message')
+    fieldValidationSpy2.error = new Error('second_arror_message')
     const sut = new ValidationComposite([
       fieldValidationSpy,
       fieldValidationSpy2
     ])
     const error = sut.validate('any_field', 'any_value')
-    expect(error).toBe('any_message')
+    expect(error).toBe('fisrt_arror_message')
   })
 })
