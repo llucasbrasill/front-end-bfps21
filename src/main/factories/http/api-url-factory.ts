469067876000e6b7206b8797f0f6c47d6840d1ee
Lucas Sodre <lucas.sodre@casion.com.br>
2021-09-10 03:04:50 -0300

    refactor: move factories to their own files

---
+++ src/main/factories/http/api-url-factory.ts
@@ -0,0 +1,3 @@
+export const makeApiUrl = (): string => {
+  return process.env.URL_LOGIN
+}
--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,22 +1,13 @@
 
 import React from 'react'
+import { makeRemoteAuthentication } from '@/main/factories/usecases/authentication/remote-authentication-factory'
 import { SignIn } from '@/presentation/pages'
-import { RemoteAuthentication } from '@/data/usecases/authentication/remote-authentication'
-import { AxiosHttpClient } from '@/infrastructure/http/axios-http-client'
-import { ValidationComposite } from '@/validation/validators'
-import { ValidationBuilder } from '@/validation/validators/builder/validation-builder'
+import { makeLoginValidation } from './login-validation-factory'
 
 export const makeLogin: React.FC = () => {
-  const url = process.env.URL_LOGIN
-  const axiosHttpClient = new AxiosHttpClient()
-  const remoteAuthentication = new RemoteAuthentication(url, axiosHttpClient)
-  const validationComposite = ValidationComposite.build([
-    ...ValidationBuilder.field('email').required().email().build(),
-    ...ValidationBuilder.field('password').required().min(6).build()
-  ])
   return (
-        <SignIn authentication={remoteAuthentication}
-            validation={validationComposite}
+        <SignIn authentication={makeRemoteAuthentication()}
+            validation={makeLoginValidation()}
         />
   )
 }
