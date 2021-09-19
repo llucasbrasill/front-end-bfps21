d6cb550b3ae10f188264472e202c74f3ae22d006
Lucas Sodre <lucas.sodre@casion.com.br>
2021-09-19 19:51:26 -0300

    feat: ensure CompareFieldsValidation return error if validation fails

---
+++ src/validation/validators/compare-fields/compare-fields-validation.ts
@@ -0,0 +1,12 @@
+import { InvalidFieldError } from '@/validation/errors'
+import { FieldValidation } from '@/validation/protocols/field-validation'
+
+export class CompareFieldValidation implements FieldValidation {
+  constructor (
+    readonly field: string,
+    private readonly valueToCompare: string) {}
+
+  validate (value: string): Error {
+    return new InvalidFieldError()
+  }
+}
