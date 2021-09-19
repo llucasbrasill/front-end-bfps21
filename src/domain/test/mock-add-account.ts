27a7b1cba07fa81eefa0050ef74e05db8270d864
Lucas Sodre <lucas.sodre@casion.com.br>
2021-09-19 18:55:08 -0300

    test: ensure RemoteAddAccount calls HttpsPostClient with correct URL

---
+++ src/domain/test/mock-add-account.ts
@@ -0,0 +1,12 @@
+
+import faker from 'faker'
+import { AddAccountParams } from '../usecases'
+
+export const mockAddAccountParams = (): AddAccountParams => {
+  const password = faker.internet.password()
+  return {
+    email: faker.internet.email(),
+    password: password,
+    passwordConfirmation: password
+  }
+}
