559a1362bcf8e484536fa465cf7c8d215f0eb18e
Lucas Sodre <lucas.sodre@casion.com.br>
2021-09-20 03:33:20 -0300

    test: ensure SignUp enable submit button if form is valid

--- src/presentation/pages/auth/signup/signup.spec.tsx
+++ src/presentation/pages/auth/signup/signup.spec.tsx
@@ -25,15 +25,43 @@ const makeSut = (params?: SutParams): SutTypes => {
   }
 }
 
-describe('SignUP Page', () => {
+describe('SignUp Page', () => {
   afterEach(cleanup)
   test('should start with initial state', () => {
     const validationError = faker.random.word()
     const { sut } = makeSut({ validationError })
     Helper.testChildCount(sut, 'errorWrapper', 0)
     Helper.testButtonIsDisabled(sut, 'submit', true)
-    Helper.testStatusForField(sut, 'email', 'Required field')
-    Helper.testStatusForField(sut, 'password', 'Required field')
+    Helper.testStatusForField(sut, 'email', validationError)
+    Helper.testStatusForField(sut, 'password', validationError)
     Helper.testStatusForField(sut, 'passwordConfirm', 'Required field')
+  })
+
+  test('should show email error if Validation fails', () => {
+    const validationError = faker.random.word()
+    const { sut } = makeSut({ validationError })
+    Helper.populateEmailField(sut, 'email')
+    Helper.testStatusForFieldEmail(sut, 'email', validationError)
+  })
+
+  test('should show password error if Validation fails', () => {
+    const validationError = faker.random.word()
+    const { sut } = makeSut({ validationError })
+    Helper.populatePasswordField(sut, 'password')
+    Helper.testStatusForFieldPassword(sut, 'password', validationError)
+  })
+
+  test('should show passwordConfirm state if Validation success', () => {
+    const { sut } = makeSut()
+    Helper.populatePasswordConfirmField(sut, 'passwordConfim')
+    Helper.testStatusForField(sut, 'passwordConfirm')
+  })
+
+  test('should enable submit button if form is valid', () => {
+    const { sut } = makeSut()
+    Helper.populateEmailField(sut, 'email')
+    Helper.populatePasswordField(sut, 'password')
+    Helper.populatePasswordConfirmField(sut, 'passwordConfim')
+    Helper.testButtonIsDisabled(sut, 'submit', false)
   })
 })
