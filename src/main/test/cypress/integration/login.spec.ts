5461c2987f9ff7ec7e06ecea2bc94e10089165df
Lucas Sodre <lucas.sodre@ehorus.com.br>
2021-10-11 11:49:09 -0300

    test: ensure SignIn begin with correct innitial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,11 @@
 describe('Login', () => {
-  it('Shoud load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('signin')
+  })
+
+  it('Shoud load with correct initial state', () => {
+    cy.getByTestId('email').should('have.attr', 'required')
+    cy.getByTestId('submitEmail').should('have.attr', 'disabled')
+    cy.getByTestId('emailStatus').should('not.have.descendants')
   })
 })
