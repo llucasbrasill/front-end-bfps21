2b237d42a7e0a045315fd8369ca4d402b7b7232e
Lucas Sodre <lucas.sodre@ehorus.com.br>
2021-10-12 01:11:15 -0300

    test: ensure SignIn present error state if form is invalid

---
+++ src/main/test/cypress/integration/signin.spec.ts
@@ -0,0 +1,19 @@
+import faker from 'faker'
+
+describe('SignIn', () => {
+  beforeEach(() => {
+    cy.visit('signin')
+  })
+
+  it('Shoud load with correct initial state', () => {
+    cy.getByTestId('email').should('have.attr', 'required')
+    cy.getByTestId('submitEmail').should('have.attr', 'disabled')
+    cy.getByTestId('emailStatus').should('not.have.descendants')
+  })
+
+  it('Shoud preset error state if form is invalid', () => {
+    cy.getByTestId('email').type(faker.random.word())
+    cy.getByTestId('emailStatus').contains('Por favor, informe um valor correto.')
+    cy.getByTestId('submitEmail').should('have.attr', 'disabled')
+  })
+})
