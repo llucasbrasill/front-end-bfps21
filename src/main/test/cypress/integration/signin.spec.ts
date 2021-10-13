bec91e778cc69c89ff07afa5d85c321f8c8864a7
Lucas Sodre <lucas.sodre@ehorus.com.br>
2021-10-13 03:52:03 -0300

    refactor: fix typo description

--- src/main/test/cypress/integration/signin.spec.ts
+++ src/main/test/cypress/integration/signin.spec.ts
@@ -7,24 +7,24 @@ describe('SignIn', () => {
     cy.visit('signin')
   })
 
-  it('Shoud load with correct initial state', () => {
+  it('Should load with correct initial state', () => {
     cy.getByTestId('email').should('have.attr', 'required')
     cy.getByTestId('submitEmail').should('have.attr', 'disabled')
     cy.getByTestId('emailStatus').should('not.have.descendants')
   })
 
-  it('Shoud preset error state if form is invalid', () => {
+  it('Should present error state if form is invalid', () => {
     cy.getByTestId('email').type(faker.random.word())
     cy.getByTestId('emailStatus').contains('Por favor, informe um valor correto.')
     cy.getByTestId('submitEmail').should('have.attr', 'disabled')
   })
 
-  it('Shoud preset valid state if step email form is valid', () => {
+  it('Should present valid state if step email form is valid', () => {
     cy.getByTestId('email').type(faker.internet.email())
     cy.getByTestId('emailStatus').should('not.have.descendants')
   })
 
-  it('Shoud preset error state if password form is invalid', () => {
+  it('Should present error state if password form is invalid', () => {
     cy.getByTestId('email').type(faker.internet.email())
     cy.getByTestId('submitEmail').click()
     cy.getByTestId('password').type(faker.datatype.string(4))
@@ -32,33 +32,41 @@ describe('SignIn', () => {
     cy.getByTestId('submitPassword').should('have.attr', 'disabled')
   })
 
-  it('Shoud preset valid state if step password form is valid', () => {
+  it('Should present valid state if step password form is valid', () => {
     cy.getByTestId('email').type(faker.internet.email())
     cy.getByTestId('submitEmail').click()
     cy.getByTestId('password').type(faker.datatype.string(6))
     cy.getByTestId('passwordStatus').should('not.have.descendants')
   })
 
-  it('Shoud preset error if invalid credentials are provided', () => {
+  it('Should present save accessToken if valid credentials are provided', () => {
+    cy.intercept('POST', '/api/login', {
+      statusCode: 200,
+      body: {
+        accessToken: faker.git.commitSha()
+      }
+    })
     cy.getByTestId('email').type(faker.internet.email())
     cy.getByTestId('submitEmail').click()
     cy.getByTestId('password').type(faker.datatype.string(6))
     cy.getByTestId('submitPassword').click()
-    cy.getByTestId('loading').should('exist')
-    cy.getByTestId('loading').should('not.exist')
-    cy.getByTestId('mainError').should('exist')
-    cy.url().should('eq', `${baseUrl}/signin`)
-  })
-
-  it('Shoud preset save accessToken if valid credentials are provided', () => {
-    cy.getByTestId('email').type('lucas@llucasbrasill.com')
-    cy.getByTestId('submitEmail').click()
-    cy.getByTestId('password').type('aaaaaa')
-    cy.getByTestId('submitPassword').click()
-    cy.getByTestId('loading').should('exist')
     cy.getByTestId('loading').should('not.exist')
     cy.getByTestId('mainError').should('not.exist')
     cy.url().should('eq', `${baseUrl}/`)
     cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
+  })
+
+  it('Should present mutiple submits', () => {
+    cy.intercept('POST', '/api/login', {
+      statusCode: 200,
+      body: {
+        accessToken: faker.git.commitSha()
+      }
+    }).as('request')
+    cy.getByTestId('email').type(faker.internet.email())
+    cy.getByTestId('submitEmail').click()
+    cy.getByTestId('password').type(faker.datatype.string(6))
+    cy.getByTestId('submitPassword').dblclick()
+    cy.get('@request.all').should('have.length', 1)
   })
 })
