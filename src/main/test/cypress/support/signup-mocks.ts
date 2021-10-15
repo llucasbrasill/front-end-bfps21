1bc4d6634cbeae426416692c7fe2cee80378be1c
Lucas Sodre <lucas.sodre@ehorus.com.br>
2021-10-15 02:06:55 -0300

    test: ensure SignUp present UnexpectedError on default error cases

--- src/main/test/cypress/support/signup-mocks.ts
+++ src/main/test/cypress/support/signup-mocks.ts
@@ -1,3 +1,4 @@
 import * as Helper from './form-mocks'
 
 export const mockEmailInUseError = (): void => Helper.mockEmailInUseError('/api/signup')
+export const mockUnexpectedError = (): void => Helper.mockUnexpectedError('POST', '/api/signup')
