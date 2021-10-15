c0e3235f74ffd1acc0e3e60454557c5259d2a392
Lucas Sodre <lucas.sodre@ehorus.com.br>
2021-10-15 02:52:49 -0300

    refactor: move duplicate code into form helper

--- src/main/test/cypress/support/form-helper.ts
+++ src/main/test/cypress/support/form-helper.ts
@@ -1,6 +1,6 @@
 import faker from 'faker'
 
-export const SimulateSubmit = (): void => {
+const SimulateSubmitAction = (): void => {
   const password = faker.internet.password()
   cy.getByTestId('name').type(faker.name.findName())
   cy.get('form > div:nth-child(1) > div > span').should('not.have.text', 'Por favor, informe um valor correto.')
@@ -11,19 +11,13 @@ export const SimulateSubmit = (): void => {
   cy.getByTestId('passwordConfirm').type(password)
   cy.get('form > div:nth-child(4) > div > span').should('not.have.text', 'Por favor, informe um valor correto.')
   cy.getByTestId('submit').should('not.have.attr', 'disabled')
+}
+export const SimulateSubmit = (): void => {
+  SimulateSubmitAction()
   cy.getByTestId('submit').click()
 }
 
 export const SimulateSubmitDoubleClick = (): void => {
-  const password = faker.internet.password()
-  cy.getByTestId('name').type(faker.name.findName())
-  cy.get('form > div:nth-child(1) > div > span').should('not.have.text', 'Por favor, informe um valor correto.')
-  cy.getByTestId('email').type(faker.internet.email())
-  cy.get('form > div:nth-child(2) > div > span').should('not.have.text', 'Por favor, informe um valor correto.')
-  cy.getByTestId('password').type(password)
-  cy.get('form > div:nth-child(3) > div > span').should('not.have.text', 'Por favor, informe um valor correto.')
-  cy.getByTestId('passwordConfirm').type(password)
-  cy.get('form > div:nth-child(4) > div > span').should('not.have.text', 'Por favor, informe um valor correto.')
-  cy.getByTestId('submit').should('not.have.attr', 'disabled')
+  SimulateSubmitAction()
   cy.getByTestId('submit').click()
 }
