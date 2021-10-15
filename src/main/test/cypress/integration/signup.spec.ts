b2298ded17e7f6439a8ce9f14c036abe1c732f03
Lucas Sodre <lucas.sodre@ehorus.com.br>
2021-10-15 01:04:07 -0300

    test: ensure SignUp present error state if form is invalid

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -1,4 +1,6 @@
 
+import faker from 'faker'
+
 describe('SignUp', () => {
   beforeEach(() => {
     cy.visit('signup')
@@ -9,6 +11,18 @@ describe('SignUp', () => {
     cy.getByTestId('email').should('have.attr', 'required')
     cy.getByTestId('password').should('have.attr', 'required')
     cy.getByTestId('passwordConfirm').should('have.attr', 'required')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+  })
+
+  it('Should present error state if form is invalid', () => {
+    cy.getByTestId('name').type(faker.datatype.string(2))
+    cy.get('form > div:nth-child(1) > div span').contains('Por favor, informe um valor correto.')
+    cy.getByTestId('email').type(faker.datatype.string())
+    cy.get('form > div:nth-child(2) > div span').contains('Por favor, informe um valor correto.')
+    cy.getByTestId('password').type(faker.datatype.string(5))
+    cy.get('form > div:nth-child(3) > div span').contains('Por favor, informe um valor correto.')
+    cy.getByTestId('passwordConfirm').type(faker.datatype.string(5))
+    cy.get('form > div:nth-child(4) > div span').contains('Por favor, informe um valor correto.')
     cy.getByTestId('submit').should('have.attr', 'disabled')
   })
 })
