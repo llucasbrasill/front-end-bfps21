7ef2f575d3bb0a0d627b64d6b9d138d6daa05ad5
Lucas Sodre <lucas.sodre@ehorus.com.br>
2021-10-15 02:17:28 -0300

    test: ensure SignUp present correct error if invalid data

--- src/main/test/cypress/support/signup-mocks.ts
+++ src/main/test/cypress/support/signup-mocks.ts
@@ -1,4 +1,6 @@
 import * as Helper from './form-mocks'
+import faker from 'faker'
 
 export const mockEmailInUseError = (): void => Helper.mockEmailInUseError('/api/signup')
 export const mockUnexpectedError = (): void => Helper.mockUnexpectedError('POST', '/api/signup')
+export const mockInvalid = (): void => Helper.mockOk('POST', '/api/signup', { invalid: faker.git.commitSha() })
